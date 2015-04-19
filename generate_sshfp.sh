4d85fbd50f67d5b82705691f075a0aaca6827d92
Andrew Rowson <github@growse.com>
2015-04-19 21:49:54 +0100

    Generate 300s sshfp records

--- generate_sshfp.sh
+++ generate_sshfp.sh
@@ -4,8 +4,8 @@ HN=$(hostname -s)
 I="1"
 for ALGO in rsa dsa ecdsa ; do
     if [ -f /etc/ssh/ssh_host_${ALGO}_key ] ; then
-        echo -n "$HN 3600 SSHFP $I 1 " ; awk '{print $2}' /etc/ssh/ssh_host_${ALGO}_key.pub | openssl base64 -d -A | openssl sha1   |cut -f2 -d' '
-        echo -n "$HN 3600 SSHFP $I 2 " ; awk '{print $2}' /etc/ssh/ssh_host_${ALGO}_key.pub | openssl base64 -d -A | openssl sha256 |cut -f2 -d' '
+        echo -n "$HN 300 SSHFP $I 1 " ; awk '{print $2}' /etc/ssh/ssh_host_${ALGO}_key.pub | openssl base64 -d -A | openssl sha1   |cut -f2 -d' '
+        echo -n "$HN 300 SSHFP $I 2 " ; awk '{print $2}' /etc/ssh/ssh_host_${ALGO}_key.pub | openssl base64 -d -A | openssl sha256 |cut -f2 -d' '
     fi
     I=$(($I+1))
 done
