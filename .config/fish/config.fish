1a3a28c1a1aa5d4b9c974543e4e93e0d8f538c92
Andrew Rowson <github@growse.com>
2021-08-08 15:18:39 +0100

    Updated fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 function fish_prompt
     powerline-rust-bare $status
 end
+
+set -U fish_user_paths $HOME/bin $HOME/.cargo/bin $HOME/.krew/bin $fish_user_paths
+export HELM_NAMESPACE="helmthings"
+
