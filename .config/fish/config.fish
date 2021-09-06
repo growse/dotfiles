6b33f57fb7eb282c36cf15751890360a5afaa0f9
Andrew Rowson <github@growse.com>
2021-09-06 09:25:27 +0100

    Remove fish

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-    powerline-rust-bare $status
-end
-
-set -U fish_user_paths $HOME/bin $HOME/.cargo/bin $HOME/.krew/bin $fish_user_paths
-export HELM_NAMESPACE="helmthings"
-
