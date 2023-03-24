04bd8bd9f4d80e60cb5f41879b5364c494be7d04
Andrew Rowson <github@growse.com>
2023-03-24 09:26:36 +0000

    Updated wezterm theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 return {
 	default_prog = {"/bin/bash"},
 	font = wezterm.font "Cascadia Mono",
-	color_scheme = "Solarized Dark (base16)",
+	color_scheme = "Builtin Solarized Dark",
 	scrollback_lines = 3500,
 	hide_tab_bar_if_only_one_tab = true
 }
